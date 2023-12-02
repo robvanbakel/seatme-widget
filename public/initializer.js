e1a7935e74cafcf8962c923906f6d4dfec7a4ea6
Rob van Bakel <robbakel@gmail.com>
2023-12-02 11:21:13 +0100

    Update initializer to render more consistently

--- public/initializer.js
+++ public/initializer.js
@@ -3,32 +3,44 @@ const widgetUrl = new URL("http://localhost:5173");
 const restaurantId = document.currentScript.getAttribute("restauraunt-id");
 widgetUrl.searchParams.append("restaurantId", restaurantId);
 
+const wrapper = document.createElement("div");
+
+wrapper.style.zIndex = 9999;
+wrapper.style.position = "fixed";
+wrapper.style.right = 0;
+wrapper.style.bottom = 0;
+wrapper.style.overflow = "hidden";
+
 const iframe = document.createElement("iframe");
 
 iframe.src = widgetUrl.href;
 
-iframe.style.position = "fixed";
 iframe.style.border = "none";
-iframe.style.zIndex = 9999;
+iframe.style.width = "416px";
+iframe.style.height = "374px";
+iframe.style.position = "absolute";
 iframe.style.right = 0;
 iframe.style.bottom = 0;
 
-document.body.appendChild(iframe);
+wrapper.appendChild(iframe);
 
 const resetIframesize = () => {
-  iframe.width = 96;
-  iframe.height = 96;
+  wrapper.style.width = "96px";
+  wrapper.style.height = "96px";
 };
 
 resetIframesize();
+
+document.body.appendChild(wrapper);
 
 window.addEventListener("message", (event) => {
   if (event.origin !== widgetUrl.origin) return;
 
   if (event.data.open) {
-    iframe.width = event.data.width;
-    iframe.height = event.data.height;
-  } else {
-    setTimeout(resetIframesize, 150);
+    wrapper.style.width = `${event.data.width}px`;
+    wrapper.style.height = `${event.data.height}px`;
+    return;
   }
+
+  setTimeout(resetIframesize, 200);
 });
