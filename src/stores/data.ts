ade24e818fe48bb0a9d04e16acf3eb64f000bdc7
Rob van Bakel <robbakel@gmail.com>
2024-04-29 21:42:56 +0200

    Add Zod validation

--- src/stores/data.ts
+++ src/stores/data.ts
@@ -8,34 +8,33 @@ type Restaurant = {
   name: string;
 };
 
-const Step1Schema = z.object({
+const ReservationSchema = z.object({
   arrivalTime: z
     .string()
     .regex(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}/, "Invalid date"),
   partySize: z.number().max(99),
   notes: z.string().optional(),
-});
-
-const Step2Schema = z.object({
   name: z.string().min(1),
-  email: z.string().email().optional(),
+  email: z.union([z.string().email(), z.literal("")]).optional(),
   phone: z
-    .string()
-    .refine(
-      (val) => {
-        return (
-          val.split("").filter((char) => !isNaN(parseInt(char))).length >= 10
-        );
-      },
-      {
-        message: "Invalid phone number",
-      }
-    )
+    .union([
+      z.string().refine(
+        (val) => {
+          return (
+            val.split("").filter((char) => !isNaN(parseInt(char))).length >= 10
+          );
+        },
+        {
+          message: "Invalid phone number",
+        }
+      ),
+      z.literal(""),
+    ])
     .optional(),
   preferredContactMethod: z.enum(["email", "phone"]),
 });
 
-type Reservation = z.infer<typeof Step1Schema & typeof Step2Schema>;
+type Reservation = z.infer<typeof ReservationSchema>;
 export type ReservationField = keyof Reservation;
 
 export const useDataStore = defineStore("data", () => {
@@ -44,12 +43,27 @@ export const useDataStore = defineStore("data", () => {
 
   const isReady = computed(() => !!restaurant.value);
 
+  const fieldErrors = computed(() => {
+    return (
+      ReservationSchema.safeParse(reservation.value).error?.formErrors
+        .fieldErrors ?? {}
+    );
+  });
+
   const isStep1Valid = computed(() => {
-    return Step1Schema.safeParse(reservation.value).success;
+    const fields: ReservationField[] = ["arrivalTime", "partySize", "notes"];
+    return fields.every((field) => !fieldErrors.value[field]?.length);
   });
 
   const isStep2Valid = computed(() => {
-    return Step2Schema.safeParse(reservation.value).success;
+    const fields: ReservationField[] = [
+      "name",
+      "email",
+      "phone",
+      "preferredContactMethod",
+    ];
+
+    return fields.every((field) => !fieldErrors.value[field]?.length);
   });
 
   const init = async () => {
@@ -86,6 +100,7 @@ export const useDataStore = defineStore("data", () => {
     restaurant,
     reservation,
     reset,
+    fieldErrors,
     isStep1Valid,
     isStep2Valid,
     submit,
