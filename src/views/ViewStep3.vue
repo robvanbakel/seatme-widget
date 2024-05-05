158ec101fea25e64311024d995ca60b40119eb54
Rob van Bakel <robbakel@gmail.com>
2024-05-05 22:08:09 +0200

    Add summary to confirmation step

--- src/views/ViewStep3.vue
+++ src/views/ViewStep3.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { CheckCircleIcon } from "@heroicons/vue/24/outline";
-
+import { useDataStore } from "@/stores/data";
 import MainButton from "@/components/MainButton.vue";
+import { ClockIcon, UserIcon } from "@heroicons/vue/20/solid";
+import dayjs from "dayjs";
+
+const dataStore = useDataStore();
 
 const emit = defineEmits<{
   close: [];
 }>();
+
+const formattedPartySize = computed(() => {
+  const partySize = dataStore.reservation.partySize;
+  if (partySize === 1) return "1 person";
+  return `${partySize} people`;
+});
+
+const formattedDate = computed(() => {
+  return dayjs(dataStore.reservation.arrivalTime).format("D MMM HH:mm");
+});
 </script>
 
 <template>
@@ -16,6 +31,24 @@ const emit = defineEmits<{
       Your reservation has successfully been made and a confirmation has been
       sent to you.
     </div>
+
+    <div class="mt-6 w-full rounded-lg bg-slate-200 p-4 text-sm shadow">
+      <h2 class="font-semibold">Reservation details</h2>
+
+      <div class="mt-2 space-y-1">
+        <div class="flex justify-center gap-2">
+          <ClockIcon class="w-4" />
+          <p>{{ formattedDate }}</p>
+        </div>
+        <div class="flex justify-center gap-2">
+          <UserIcon class="w-4" />
+          <p>
+            {{ formattedPartySize }}
+          </p>
+        </div>
+      </div>
+    </div>
+
     <div class="mt-6 flex justify-center">
       <MainButton small @click="emit('close')" label="Close" />
     </div>
