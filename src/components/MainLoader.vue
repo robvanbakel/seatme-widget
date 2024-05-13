3f5e7615ec2ee86dc241ff06fd79e73e06ee2f11
Rob van Bakel <robbakel@gmail.com>
2024-05-13 13:03:50 +0200

    Remove MainLoader size property

--- src/components/MainLoader.vue
+++ src/components/MainLoader.vue
@@ -3,12 +3,11 @@ import { ArrowPathIcon } from "@heroicons/vue/24/outline";
 
 defineProps<{
   selfCenter?: boolean;
-  size?: number;
 }>();
 </script>
 
 <template>
   <div :class="{ 'grid place-items-center': selfCenter }">
-    <ArrowPathIcon :size="size || 36" class="animate-spin" />
+    <ArrowPathIcon class="animate-spin" />
   </div>
 </template>
